10b5a0522d5fa545e4fabf91cdab1ba94b55d283
angelos <angelos.kapsimanis@madvertise.com>
2013-12-20 13:14:43 +0100

    Added NoteModel code

---
+++ model/ChapterModel.js
@@ -0,0 +1,27 @@
+Guidebook.service('ChapterModel', funtion() {
+
+    this.getChapters = function() {
+      return [{
+        id: 0,
+        title: 'Chapter 1: What is AngularJS?',
+        summary: 'Find out what separates AngularJS from...'
+      }, {
+        id: 1,
+        title: 'Chapter 2: HelloWorld',
+        summary: 'Learn how to get up and running with...'
+      }, {
+        id: 2,
+        title: 'Chapter 3: Quickstart',
+        summary: 'Brush up on the Model-View-Controller...'
+      }, {
+        id: 3,
+        title: 'Chapter 4: Key AngularJS Features',
+        summary: 'Discover the strengths of AngularJS'
+      }, {
+        id: 4,
+        title: 'Chapter 5',
+        summary: 'Get to know the top contributors...'
+      }
+    ]};
+
+});
